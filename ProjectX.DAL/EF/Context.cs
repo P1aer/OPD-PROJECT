48e7160e387af9460414a2ca521893bab5e91c8b
Ivan Eroshin <itseroshin@gmail.com>
2022-05-01 14:43:14 +0300

    add entities

---
+++ ProjectX.DAL/EF/Context.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectX.DAL.Entities;
+
+namespace ProjectX.DAL.EF
+{
+    internal class Context : DbContext
+    {
+        public DbSet<Homework> Homeworks { get; set; }
+        public DbSet<Lecture> Lectures { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public Context()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ ProjectX.DAL/Entities/Homework.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectX.DAL.Entities
+{
+    internal class Homework
+    {
+        public int HomeworkID { get; set; }
+
+        public short? Grade { get; set; }
+
+        public int StudentStudentID { get; set; }
+        public Student Student { get; set; }
+
+        public int LectureLectureID { get; set; }
+        public Lecture Lecture { get; set; }
+    }
+}
