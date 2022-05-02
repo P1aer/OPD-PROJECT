5a7c646c3ed404ed457225d98d6aabe8fbe3753e
Ilya Osipov <arhoangel@bk.ru>
2022-05-02 13:51:41 +0300

    repos done

---
+++ ProjectX.DAL/Repositories/StudentRepository.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectX.DAL.EF;
+using ProjectX.DAL.Entities;
+using ProjectX.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectX.DAL.Repositories
+{
+    internal class StudentRepository : IRepository<Student>
+    {
+        private Context db;
+
+        public StudentRepository()
+        {
+            db = new Context();
+        }
+        public void Create(Student item)
+        {
+            db.Students.Add(item);
+        }
+
+        public void Delete(int id)
+        {
+            Student? student = db.Students.Find(id);
+            if (student != null)
+            {
+                db.Students.Remove(student);
+            }
+           
+        }
+        private bool disposed = false;
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public IEnumerable<Student> GetAll()
+        {
+             return db.Students;
+        }
+
+        public Student? GetOne(int id)
+        {
+            return db.Students.Find(id);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Update(Student item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+        }
+    }
+}
---
+++ ProjectX.DAL/Repositories/TeacherRepository.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectX.DAL.EF;
+using ProjectX.DAL.Entities;
+using ProjectX.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectX.DAL.Repositories
+{
+    internal class TeacherRepository : IRepository<Teacher>
+    {
+        private Context db;
+
+        public TeacherRepository()
+        {
+            db = new Context();
+        }
+        public void Create(Teacher item)
+        {
+            db.Teachers.Add(item);
+        }
+
+        public void Delete(int id)
+        {
+            Teacher? teacher = db.Teachers.Find(id);
+            if (teacher != null)
+            {
+                db.Teachers.Remove(teacher);
+            }
+
+        }
+        private bool disposed = false;
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public IEnumerable<Teacher> GetAll()
+        {
+            return db.Teachers;
+        }
+
+        public Teacher? GetOne(int id)
+        {
+            return db.Teachers.Find(id);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Update(Teacher item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+        }
+    }
+}
+
